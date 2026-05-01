5c252fe5cd1859d8d703ab8dd5fb7e9c88208dae
Joao Goncalves <goncalves.joao@gmail.com>
2026-05-01 22:31:41 +0900

    breaking down typography into individual files

--- tests/examples/docs-page-example.spec.ts
+++ tests/examples/docs-page-example.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.use({ viewport: { width: 400, height: 200 } });
 
-test('Demo/Semantics - visual snapshot', async ({ page }) => {
-  await page.goto('/iframe.html?id=demo-docspageexample--docs-page-example');
+test('Examples/DocsPage - visual snapshot', async ({ page }) => {
+  await page.goto('/iframe.html?id=examples-docspage--docs-page');
   const main = page.locator('main');
 
   await expect(main).toBeVisible();
--- tests/examples/semantics.spec.ts
+++ tests/examples/semantics.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.use({ viewport: { width: 400, height: 200 } });
 
-test('Demo/Semantics - visual snapshot', async ({ page }) => {
-  await page.goto('/iframe.html?id=demo-semantics--semantics');
+test('Examples/Semantics - visual snapshot', async ({ page }) => {
+  await page.goto('/iframe.html?id=examples-semantics--semantics');
   const main = page.locator('main');
 
   await expect(main).toBeVisible();
